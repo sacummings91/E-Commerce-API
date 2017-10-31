82be53f2cc85d1fca834d3551f0a514dc96c8899
sacummings91 <sacummings91@gmail.com>
2017-10-31 10:14:59 -0700

    items router basic functionality working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Boom = require('boom');
+const UnauthorizedError = require('express-jwt/lib/errors/UnauthorizedError');
 
 const server = express();
 
@@ -7,6 +9,20 @@ const itemsRouter = require('./lib/instances/itemsRouter');
 
 server.use(bodyParser.json());
 server.use(itemsRouter);
+
+server.all('*', (req, res, next) => res.sendStatus(404));
+
+server.use((err, req, res, next) => {
+  console.log(err); // eslint-disable-line no-console
+  if (err instanceof UnauthorizedError || err.typeof === Boom.unauthorized) {
+    error = Boom.unauthorized(error.message, ['Bearer']);
+  }
+  if (!err.isBoom) error = Boom.badImplementation();
+  res
+    .set(err.output.headers)
+    .status(err.output.statusCode)
+    .json(err.output.payload);
+});
 
 const port =
   process.env.PORT && /^\d+$/.test(process.env.PORT)
