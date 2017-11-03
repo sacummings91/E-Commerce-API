fece836ed2f5dc321b3b2639275667adfc614feb
sacummings91 <sacummings91@gmail.com>
2017-11-03 13:50:09 -0700

    signup and login implemented"

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const { JWT_KEY } = require('./env');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const Boom = require('boom');
 const cors = require('cors');
+const jwt = require('express-jwt');
 const UnauthorizedError = require('express-jwt/lib/errors/UnauthorizedError');
 
 const server = express();
@@ -10,13 +13,33 @@ const itemsRouter = require('./lib/instances/itemsRouter');
 const usersRouter = require('./lib/instances/usersRouter');
 const favoritesRouter = require('./lib/instances/favoritesRouter');
 const ordersRouter = require('./lib/instances/ordersRouter');
+const tokenRouter = require('./lib/instances/tokenRouter');
 
 server.use(bodyParser.json());
 server.use(cors());
+server.use(
+  jwt({
+    secret: JWT_KEY,
+    requestProperty: 'jwt.payload',
+    credentialsRequired: false,
+    audiance: 'ecommerce',
+    issuer: 'ecommerce'
+  })
+);
+server.use((req, res, next) => {
+  const authenticatedUserId = req.jwt ? req.jwt.payload.sub : undefined;
+  req.authenticatedUserId =
+    Number.isFinite(authenticatedUserId) && authenticatedUserId > 0
+      ? authenticatedUserId
+      : null;
+  next();
+});
+
 server.use(itemsRouter);
 server.use(usersRouter);
 server.use(favoritesRouter);
 server.use(ordersRouter);
+server.use(tokenRouter);
 
 server.all('*', (req, res, next) => res.sendStatus(404));
 
